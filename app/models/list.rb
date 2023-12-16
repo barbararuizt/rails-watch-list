fb5bd051c8f8eaaab8909cafa6b9e13937015927
Barbara Ruiz <barbararuiz456@outlook.com>
2023-12-16 14:49:05 -0600

    models done

---
+++ app/models/list.rb
@@ -0,0 +1,10 @@
+class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+  
+  validates_associated :movies
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [] do
+    resources :reviews, only: [:new, :create]
+  end
+
+
+  # resources :restaurants, only: [:index, :show, :new, :create] do
+  #   resources :reviews, only: [:new, :create]
+  # end
 end
